315f1877e37949b116b83851e665c4b3c4128c52
Dale McNair <dockit@gmail.com>
2017-01-23 17:21:35 +1100

    Added about page content

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -11,8 +11,19 @@ const React = require('react');
 const About = function (props) {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to About</p>
+      <h1 className="text-center">About</h1>
+      <p>This is a weird weather app made with React.</p>
+      <p>
+        Here are some tools used:
+      </p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a>
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a> - Weather API
+        </li>
+      </ul>
     </div>
   );
 };
