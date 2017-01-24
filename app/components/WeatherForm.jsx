ee2a12a2874765f4a0d0e73c7345273f88b253fc
Dale McNair <dockit@gmail.com>
2017-01-24 15:26:24 +1100

    Finalised app, fixed routing

--- app/components/WeatherForm.jsx
+++ app/components/WeatherForm.jsx
@@ -4,7 +4,7 @@ var WeatherForm = React.createClass({
   onFormSubmit: function(e) {
     e.preventDefault();
 
-    var location = this.refs.location.value;
+    const location = this.refs.location.value;
     if (location.length > 0) {
       this.refs.location.value = '';
       this.props.onSearch(location);
